d82370f94b05645937eff3c3cba89a2dba31aea9
Marina Frankin <marinafrankin2005@gmail.com>
2024-11-01 16:46:27 -0300

    01/11/2024

---
+++ js/Aula01.js
@@ -0,0 +1,27 @@
+//Function declaration
+
+function sayHello () {
+    console.log('Hello')
+}
+
+sayHello()
+
+function sayHelloTo(name) {
+    console.log('Hello ' + name)
+}
+
+sayHelloTo('Marina')
+
+function returnHi() {
+    return 'Hi!'
+}
+
+let greeting = returnHi()
+console.log(freeting)
+console.log(returnHi())
+
+function returnHiTo(name) {
+    return `Hi ${name}!`
+}
+
+console.log(returnHiTo('Maria'))

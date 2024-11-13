94fdc4c60c52714bfe33210b09d7ebe77bbaa83a
Marina Frankin <marinafrankin2005@gmail.com>
2024-11-13 17:54:23 -0300

    13/11/2024

---
+++ js/Aula32.js
@@ -0,0 +1,23 @@
+function funcionarOuNao(valor, chanceErro){
+    return new Promise((resolve, reject) => {
+        try{
+        if(Math.random() < chanceErro){
+            reject('Ocorreu um erro!')
+        } else {
+            resolve(valor)
+        }
+        } catch(e){
+            resolve(e)
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.9)
+    .then(v => `Valor: ${v}`)
+    .then(
+        v => console.log(v),
+        err => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(() => console.log('Quase Fim!'))
+    .catch(err => console.log(`Erro: ${err}`))
+    .then(() => console.log(`Fim!`))
